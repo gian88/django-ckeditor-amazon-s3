6ec4e7333d14a7d83abf43f021f83986699bfb95
gian88 <gian88@hotmail.com>
2013-08-15 15:56:59 -0430

    Fix a problem the authentication the following url upload and browse making safe url

--- ckeditor/urls.py
+++ ckeditor/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib import admin
+from ckeditor import views
 
 urlpatterns = patterns(
     '',
-    url(r'^upload/', 'ckeditor.views.upload', name='ckeditor_upload'),
-    url(r'^browse/', 'ckeditor.views.browse', name='ckeditor_browse'),
+    url(r'^upload/', admin.site.admin_view(views.upload), name='ckeditor_upload'),
+    url(r'^browse/', admin.site.admin_view(views.browse), name='ckeditor_browse'),
 )
