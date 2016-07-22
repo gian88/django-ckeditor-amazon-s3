74c3fb687a7763ad91415155cbce4eae9486171e
Gianfranco Lemmo <gian88@gmail.com>
2016-07-22 13:40:05 -0400

    Fix import for work in Django 1.8

--- ckeditor/widgets.py
+++ ckeditor/widgets.py
@@ -6,7 +6,7 @@ from django.utils.safestring import mark_safe
 from django.utils.html import conditional_escape
 from django.utils.encoding import force_unicode
 from django.core.exceptions import ImproperlyConfigured
-from django.forms.util import flatatt
+from django.forms.utils import flatatt
 import json
 
 
